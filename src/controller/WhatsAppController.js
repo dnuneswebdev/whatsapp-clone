55c78cb5ba67f68d58ba58e478efda2bd837873b
Daniel <dnuneswebdev@gmail.com>
2018-12-05 11:01:01 -0200

    Input message Events

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -206,6 +206,44 @@ class WhatsAppController {
     });
 
     // --------------------------------------------------
+    // INPUT MESSAGE
+    // --------------------------------------------------
+    this.el.inputText.on('keyup', e => {
+      if(this.el.inputText.innerHTML.length) {
+        this.el.inputPlaceholder.hide();
+        this.el.btnSendMicrophone.hide();
+        this.el.btnSend.show()
+      }
+      else {
+        this.el.inputPlaceholder.show();
+        this.el.btnSendMicrophone.show();
+        this.el.btnSend.hide()
+      }
+    });
+
+    this.el.inputText.on('keypress', e => {
+      if(e.key === 'Enter' && !e.shiftKey) {
+        e.preventDefault();
+        this.el.btnSend.click();
+      }
+    });
+
+    this.el.btnEmojis.on('click', e => {
+      this.el.panelEmojis.toggleClass('open')
+    });
+
+    this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+      emoji.on('click', e => {
+        console.log(emoji.dataset.unicode)
+      })
+    });
+
+    this.el.btnSend.on('click', e => {
+      console.log(this.el.inputText.innerHTML)
+    });
+
+
+    // --------------------------------------------------
     // MICROPHONE EVENTS
     // --------------------------------------------------
     this.el.btnSendMicrophone.on('click', e => {
